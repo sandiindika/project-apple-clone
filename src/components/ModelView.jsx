30876a0bb8d9f568aadb94925c128641e3d32c0b
Sandi Indika Saputra <sandidikaputra@gmail.com>
2024-08-07 00:32:53 +0700

    feat: Add model for showcase products

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,46 @@
+import {Html, OrbitControls, PerspectiveCamera, View} from "@react-three/drei"
+
+import * as THREE from "three";
+import {Lights} from "./Lights.jsx";
+import {Suspense} from "react";
+import {Loader} from "./Loader.jsx";
+import {IPhone} from "./IPhone.jsx";
+
+// eslint-disable-next-line react/prop-types
+export const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
+    return (
+        <View
+            index={index}
+            id={gsapType}
+            className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+        >
+            {/* eslint-disable-next-line react/no-unknown-property */}
+            <ambienLight intensity={0.3} />
+
+            <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+            <Lights />
+
+            {/* eslint-disable-next-line react/prop-types */}
+            <OrbitControls onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+            />
+
+            {/* eslint-disable-next-line react/no-unknown-property */}
+            <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0,0,0]}>
+                <Suspense fallback={<Loader />}>
+                    <IPhone
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size}
+                    />
+                </Suspense>
+            </group>
+        </View>
+    )
+}
