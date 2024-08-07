973ba2e9bcae2923bdb77e207972a76280d64836
Sandi Indika Saputra <sandidikaputra@gmail.com>
2024-08-07 11:27:46 +0700

    feat: Add How it works section and component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Hero} from "./components/Hero.jsx";
 import {Highlights} from "./components/Highlights.jsx";
 import {Model} from "./components/Model.jsx";
 import {Features} from "./components/Features.jsx";
+import {HowItWorks} from "./components/HowItWorks.jsx";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <Highlights />
             <Model />
             <Features />
+            <HowItWorks />
         </main>
     )
 }
